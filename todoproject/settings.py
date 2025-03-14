c6be702ceeb7712b4610cd4995c4a1513af20d9b
Eren Çit <erean.55.44@outlook.com>
2025-03-15 00:51:18 +0300

    Added,gitignore and updated settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -12,15 +12,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv  import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!02kf4ep00u)c73bjx55o=piaeir+t66-jypa-2cxubj6#t1p)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
